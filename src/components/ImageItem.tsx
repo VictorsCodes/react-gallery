182efeec042e24b4b1e1413a5822bacaabd1cb8d
Victor <jhiorgenes.santos@gmail.com>
2022-07-20 19:18:16 -0300

    Alteracao na imagem

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const ImageItem = ({ name, url }: Props) => {
   return (
     <GridItem h='100%' boxShadow='lg' bg='gray.100' p='2' rounded='md'>
-      <Image fit='cover' src={url} alt={name} borderRadius='sm' />
+      <Image fit='cover' maxH='300px' src={url} alt={name} borderRadius='sm' />
       <Text align='center' fontSize='xs' mt={4}>
         {name}
       </Text>
